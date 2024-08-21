ed98b0b8c36e321acd458dd1236522b5797746ff
akash <akashdeep9226@gmail.com>
2024-08-22 00:52:08 +0530

    fixes

--- src/idb.js
+++ src/idb.js
@@ -1,54 +1,61 @@
 import { openDB } from 'idb';
 
 const DB_NAME = 'offlineRequestsDB';
+const DB_VERSION = 1;
+
 const STORE_NAME = 'requests';
 
 export async function initDB() {
-  return openDB(DB_NAME, 1, {
+  return openDB(DB_NAME, DB_VERSION, {
     upgrade(db) {
-      if (!db.objectStoreNames.contains(STORE_NAME)) {
-        db.createObjectStore(STORE_NAME, { keyPath: 'id', autoIncrement: true });
-      }
+        if (!db.objectStoreNames.contains('posts')) {
+            db.createObjectStore('posts', { keyPath: 'id', autoIncrement: true });
+          }
+          
+          // Create another object store for comments
+          if (!db.objectStoreNames.contains('users')) {
+            db.createObjectStore('users', { keyPath: 'id', autoIncrement: true });
+          }
     },
   });
 }
 
 export async function savePostsToDB(posts) {
     const db = await initDB();
-    const tx = db.transaction(STORE_NAME, 'readwrite');
-    const store = tx.objectStore(STORE_NAME);
+    const tx = db.transaction('posts', 'readwrite');
+    const store = tx.objectStore('posts');
     posts.forEach((post) => store.put(post));
     await tx.done;
   }
   
   export async function getPostsFromDB() {
     const db = await initDB();
-    return await db.getAll(STORE_NAME);
+    return await db.getAll('posts');
   }
   
   export async function clearPostsFromDB() {
     const db = await initDB();
-    const tx = db.transaction(STORE_NAME, 'readwrite');
-    await tx.objectStore(STORE_NAME).clear();
+    const tx = db.transaction('posts', 'readwrite');
+    await tx.objectStore('posts').clear();
     await tx.done;
   }
 
-export async function saveRequest(request) {
+export async function saveRequest(post) {
   const db = await initDB();
-  const tx = db.transaction(STORE_NAME, 'readwrite');
-  await tx.objectStore(STORE_NAME).add(request);
+  const tx = db.transaction('posts', 'readwrite');
+  await tx.objectStore('posts').add(post);
   await tx.done;
 }
 
 export async function getAllRequests() {
   const db = await initDB();
-  return db.transaction(STORE_NAME).objectStore(STORE_NAME).getAll();
+  return db.transaction('posts').objectStore('posts').getAll();
 }
 
 export async function clearRequest(id) {
   const db = await initDB();
-  const tx = db.transaction(STORE_NAME, 'readwrite');
-  await tx.objectStore(STORE_NAME).delete(id);
+  const tx = db.transaction('posts', 'readwrite');
+  await tx.objectStore('posts').delete(id);
   await tx.done;
 }
 
@@ -57,20 +64,20 @@ export async function clearRequest(id) {
 
 export async function saveUsersToDB(users) {
     const db = await initDB();
-    const tx = db.transaction(STORE_NAME, 'readwrite');
-    const store = tx.objectStore(STORE_NAME);
+    const tx = db.transaction('users', 'readwrite');
+    const store = tx.objectStore('users');
     users.forEach((user) => store.put(user));
     await tx.done;
   }
   
   export async function getUsersFromDB() {
     const db = await initDB();
-    return await db.getAll(STORE_NAME);
+    return await db.getAll('users');
   }
   
   export async function clearUsersFromDB() {
     const db = await initDB();
-    const tx = db.transaction(STORE_NAME, 'readwrite');
-    await tx.objectStore(STORE_NAME).clear();
+    const tx = db.transaction('users', 'readwrite');
+    await tx.objectStore('users').clear();
     await tx.done;
   }
--- src/sw.js
+++ src/sw.js
@@ -2,6 +2,7 @@
 import { cleanupOutdatedCaches, createHandlerBoundToURL, precacheAndRoute } from 'workbox-precaching'
 import { clientsClaim } from 'workbox-core'
 import { NavigationRoute, registerRoute } from 'workbox-routing'
+import { getAllRequests } from './idb'
 
 // self.__WB_MANIFEST is the default injection point
 precacheAndRoute(self.__WB_MANIFEST)
